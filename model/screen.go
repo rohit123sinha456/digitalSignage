5750569a7a22f5068635652d8c0d1819a7f36566
Rohit <rohit@gmail.com>
2024-05-24 01:35:09 +0530

    modified:   model/screen.go

--- model/screen.go
+++ model/screen.go
@@ -2,10 +2,10 @@ package DataModel
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-// type ScreenBlock struct {
-// 	BlockName     string             `bson:"blockname,omitempty"`
-// 	ContentListID primitive.ObjectID `bson:"contentlistid,omitempty"`
-// }
+type ScreenBlock struct {
+	BlockName     string             `bson:"blockname,omitempty"`
+	ContentListID primitive.ObjectID `bson:"contentlistid,omitempty"`
+}
 
 type Screen struct {
 	ID       primitive.ObjectID `bson:"_id"`
