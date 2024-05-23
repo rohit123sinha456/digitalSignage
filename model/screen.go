a4ff29fe8f69c9f3e2e144a4bda8c366f12b9cf3
Rohit <rohit@gmail.com>
2024-05-24 01:21:40 +0530

    Updating Screen model

--- model/screen.go
+++ model/screen.go
@@ -2,14 +2,16 @@ package DataModel
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-type ScreenBlock struct {
-	BlockName     string             `bson:"blockname,omitempty"`
-	ContentListID primitive.ObjectID `bson:"contentlistid,omitempty"`
-}
+// type ScreenBlock struct {
+// 	BlockName     string             `bson:"blockname,omitempty"`
+// 	ContentListID primitive.ObjectID `bson:"contentlistid,omitempty"`
+// }
 
 type Screen struct {
-	ID          primitive.ObjectID `bson:"_id"`
-	Name        string             `bson:"name"`
-	Location    string             `bson:"location"`
-	Screenblock []ScreenBlock      `bson:"screenblock"`
+	ID       primitive.ObjectID `bson:"_id"`
+	Name     string             `bson:"name"`
+	Location string             `bson:"location"`
+	// Screenblock []ScreenBlock      `bson:"screenblock"`
+	Screenblock string `bson:"screenblock"`
+	Screencode  string `bson:"screencode"`
 }
